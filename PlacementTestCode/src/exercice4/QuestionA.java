f4e6a50dbcc0143346e5c617616920e5599bfb31
Fahd Gamal <fgamal@sqli.com>
2018-02-07 10:29:15 +0000

    Question A Exercice 4 added

---
+++ PlacementTestCode/src/exercice4/QuestionA.java
@@ -0,0 +1,14 @@
+package exercice4;
+
+public class QuestionA {
+
+	public static void main(String[] args) {
+		System.out.println("|O|O|X|");
+		System.out.println("|O|X|X|");
+		System.out.println("|X| |O|");
+		
+		
+
+	}
+
+}
